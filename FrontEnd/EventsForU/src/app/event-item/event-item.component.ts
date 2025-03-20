e780611bf869c23b46fbef1a30fac6d705f6f0ae
ratislavka <65152341+ratislavka@users.noreply.github.com>
2025-03-20 19:54:33 +0500

    Angular initialization
    created angular files and some components

---
+++ FrontEnd/EventsForU/src/app/event-item/event-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-item',
+  imports: [],
+  templateUrl: './event-item.component.html',
+  styleUrl: './event-item.component.css'
+})
+export class EventItemComponent {
+
+}
