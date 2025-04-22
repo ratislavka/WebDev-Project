8ebfe04104b4866b9d8475c26241c512bfd8fb27
Abzal <k4bdoldaev@gmail.com>
2025-04-22 21:05:48 +0500

    fix: deleted event_booking directory

--- BackEnd/event_booking/main_page/admin.py
+++
@@ -1,7 +0,0 @@
-from main_page.models import Event, User, Booking, Ticket
-from django.contrib import admin
-
-admin.site.register(Event)
-admin.site.register(User)
-admin.site.register(Booking)
-admin.site.register(Ticket)
